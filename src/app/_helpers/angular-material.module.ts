e6466402e63c75d6ab3305b4f71a60ff727cc986
Areksbi <36035819+Areksbi@users.noreply.github.com>
2019-12-31 15:25:00 +0100

    Add footer and grid system

--- src/app/_helpers/angular-material.module.ts
+++ src/app/_helpers/angular-material.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
   exports: [
+    MatGridListModule,
     MatToolbarModule,
   ]
 })
