747b383079de2c9eb0a6d1ca87cded1dbaced843
Areksbi <36035819+Areksbi@users.noreply.github.com>
2019-12-31 16:38:59 +0100

    Add tabs for forms

--- src/app/_helpers/angular-material.module.ts
+++ src/app/_helpers/angular-material.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   exports: [
+    MatCardModule,
+    MatTabsModule,
     MatToolbarModule,
   ]
 })
