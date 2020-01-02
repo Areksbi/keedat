741f6eea748a820375781a517b72957702d32f1a
Areksbi <36035819+Areksbi@users.noreply.github.com>
2020-01-02 14:51:06 +0100

    Add toggle password visibility

--- src/app/_helpers/angular-material.module.ts
+++ src/app/_helpers/angular-material.module.ts
@@ -5,11 +5,13 @@ import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   exports: [
     MatButtonModule,
     MatCardModule,
+    MatIconModule,
     MatInputModule,
     MatProgressSpinnerModule,
     MatTabsModule,
