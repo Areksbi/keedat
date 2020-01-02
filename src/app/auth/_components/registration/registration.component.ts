06683b560d7f0af9202039a1f7d1c9809a075266
Areksbi <36035819+Areksbi@users.noreply.github.com>
2020-01-02 14:40:07 +0100

    Add registration template

--- src/app/auth/_components/registration/registration.component.ts
+++ src/app/auth/_components/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -6,10 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
+  public isLoading = false;
+  public registerForm: FormGroup;
+  public submitted = false;
 
-  constructor() { }
+  constructor(
+  ) {
+  }
 
   ngOnInit() {
+    this.registerForm = new FormGroup({
+      email: new FormControl(null, {
+        validators: [
+          Validators.required
+        ]
+      }),
+      password: new FormControl(null, {
+        validators: [
+          Validators.minLength(8),
+          Validators.required
+        ]
+      })
+    });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    this.isLoading = true;
   }
 
 }
