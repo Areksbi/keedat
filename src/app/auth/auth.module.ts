3baf87aef5664be234259473da9eed2ef1b24d0f
Areksbi <36035819+Areksbi@users.noreply.github.com>
2020-01-02 14:28:39 +0100

    Add login template

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AngularMaterialModule } from '../_helpers/angular-material.module';
 import { AuthContainer } from './auth.container';
@@ -17,6 +19,8 @@ import { LoginComponent, RegistrationComponent } from './_components';
   imports: [
     AngularMaterialModule,
     CommonModule,
+    HttpClientModule,
+    ReactiveFormsModule,
   ],
 })
 export class AuthModule {
