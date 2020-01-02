c8c24d89bdf034d659410bc7aaf6e9c0ba3e97fe
Areksbi <36035819+Areksbi@users.noreply.github.com>
2020-01-02 13:16:02 +0100

    Add login and registration components

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AngularMaterialModule } from '../_helpers/angular-material.module';
 import { AuthContainer } from './auth.container';
+import { LoginComponent, RegistrationComponent } from './_components';
 
 @NgModule({
   declarations: [
     AuthContainer,
+    LoginComponent,
+    RegistrationComponent,
   ],
   exports: [
     AuthContainer,
