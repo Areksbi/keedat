7b06c65dda0c03a3dbf476d28b242c0ead7e5f93
Areksbi <36035819+Areksbi@users.noreply.github.com>
2020-01-10 19:06:18 +0100

    Add translate module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AngularMaterialModule } from './_helpers/angular-material.module';
 import { AppComponent } from './app.component';
@@ -23,6 +27,14 @@ import { FooterComponent, HeaderComponent } from './_components';
     AuthModule,
     BrowserAnimationsModule,
     BrowserModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [
 
@@ -30,4 +42,8 @@ import { FooterComponent, HeaderComponent } from './_components';
 })
 export class AppModule {
 
+}
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
 }
