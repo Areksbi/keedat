be5355a5fd6f8feb49c42c417213ac0c5d39efda
Areksbi <36035819+Areksbi@users.noreply.github.com>
2020-01-10 19:06:18 +0100

    Add translate module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import { TranslateService } from '@ngx-translate/core';
+
+import defaultLanguage from './../assets/i18n/en.json';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-pilot-data-selling';
+
+  constructor(private translate: TranslateService) {
+    translate.setTranslation('en', defaultLanguage);
+    translate.setDefaultLang('en');
+  }
 }
