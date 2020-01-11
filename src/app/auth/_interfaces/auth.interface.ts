c37a6f15217373ed6500c5e71245bbbb163b14f0
Areksbi <36035819+Areksbi@users.noreply.github.com>
2020-01-11 19:42:04 +0100

    Clean interfaces

--- src/app/auth/_interfaces/auth.interface.ts
+++ src/app/auth/_interfaces/auth.interface.ts
@@ -1,7 +1,19 @@
-export interface AuthInterface {
+export interface RequestRegistrationInterface {
   email: string;
   password: string;
   privacyPolicyConsent: boolean;
   recaptchaToken: string;
   userAgreementConsent: boolean;
+}
+
+export interface ResponseRegistrationInterface {
+  message: string;
+  result: {
+    _id: string;
+    email: string;
+    password: string;
+    privacyPolicyConsent: boolean;
+    recaptchaToken: string;
+    userAgreementConsent: boolean;
+  }
 }
