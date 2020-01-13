f156f1ac01fa345d96adca05997f12d380c3be7d
Areksbi <36035819+Areksbi@users.noreply.github.com>
2020-01-13 09:07:44 +0100

    Add account and privacy policy modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { PrivacyPolicyComponent } from './_components';
 import { urls } from './_constants/urls.constant';
-
 
 const routes: Routes = [
   {
-    component: PrivacyPolicyComponent,
     path: urls.PRIVACY_POLICY,
+    loadChildren: () => import('./privacy-policy/privacy-policy.module').then(m => m.PrivacyPolicyModule),
+  },
+  {
+    path: 'account',
+    loadChildren: () => import('./account/account.module').then(m => m.AccountModule),
   },
   {
     path: '**',
