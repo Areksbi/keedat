36ef70de058665d8b1f5f57d77b9cef5015aab1a
Areksbi <36035819+Areksbi@users.noreply.github.com>
2020-01-18 10:35:37 +0100

    Add footer

--- src/app/_components/footer/footer.component.ts
+++ src/app/_components/footer/footer.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { links } from '../../_constants/links.constant';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class FooterComponent {
+  public currentYear: number = new Date().getFullYear();
+  public links = links;
 }
