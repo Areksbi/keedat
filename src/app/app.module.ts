3a8d36db55b7e582b4ad67aaee605c44293db982
Areksbi <36035819+Areksbi@users.noreply.github.com>
2020-02-02 22:56:24 +0100

    Add more advanced store structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
-import { reducers, metaReducers } from './reducers';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { StoreModule } from '@ngrx/store';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AngularMaterialModule } from './_helpers/angular-material.module';
 import { AppComponent } from './app.component';
+import { AppEffects } from './_store/effects/app.effects';
 import { AppRoutingModule } from './app-routing.module';
 import { AuthModule } from './auth/auth.module';
 import { AuthInterceptor } from './auth/_helpers/auth.interceptor';
+import { environment } from '../environments/environment';
 import { ErrorComponent, FooterComponent, HeaderComponent } from './_components';
 import { ErrorInterceptor } from './_helpers/error.interceptor';
+import { reducers, metaReducers } from './_store/reducers';
 
 @NgModule({
   bootstrap: [
@@ -23,8 +27,8 @@ import { ErrorInterceptor } from './_helpers/error.interceptor';
   declarations: [
     AppComponent,
     ErrorComponent,
-    HeaderComponent,
     FooterComponent,
+    HeaderComponent,
   ],
   entryComponents: [
     ErrorComponent,
@@ -35,7 +39,15 @@ import { ErrorInterceptor } from './_helpers/error.interceptor';
     AuthModule,
     BrowserAnimationsModule,
     BrowserModule,
+    EffectsModule.forRoot([AppEffects]),
     HttpClientModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers,
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true,
+      }
+    }),
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
@@ -43,13 +55,7 @@ import { ErrorInterceptor } from './_helpers/error.interceptor';
         deps: [HttpClient],
       },
     }),
-    StoreModule.forRoot(reducers, {
-      metaReducers,
-      runtimeChecks: {
-        strictStateImmutability: true,
-        strictActionImmutability: true
-      }
-    }),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
   ],
   providers: [
     {
