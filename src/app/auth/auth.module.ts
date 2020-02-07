1d48c1cab4c8f373f413f8ed3e2d00ba06caed27
Areksbi <36035819+Areksbi@users.noreply.github.com>
2020-02-07 22:13:27 +0100

    Move recaptcha to core

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,7 +4,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { EffectsModule } from '@ngrx/effects';
-import { RECAPTCHA_V3_SITE_KEY, RecaptchaV3Module } from 'ng-recaptcha';
 import { StoreModule } from '@ngrx/store';
 import { TranslateModule } from '@ngx-translate/core';
 
@@ -12,7 +11,6 @@ import { AngularMaterialModule } from '../_helpers';
 import { AuthContainer } from './auth.container';
 import { AuthEffects } from './_store/effects/auth.effects';
 import { authFeatureKey, authReducer } from './_store/reducers/auth.reducer';
-import { environment } from '../../environments/environment';
 import { LoginComponent, RegistrationComponent } from './_components';
 
 @NgModule({
@@ -30,15 +28,8 @@ import { LoginComponent, RegistrationComponent } from './_components';
     EffectsModule.forFeature([AuthEffects]),
     HttpClientModule,
     ReactiveFormsModule,
-    RecaptchaV3Module,
     StoreModule.forFeature(authFeatureKey, authReducer),
     TranslateModule
-  ],
-  providers: [
-    {
-      provide: RECAPTCHA_V3_SITE_KEY,
-      useValue: environment.recaptcha
-    },
   ],
 })
 export class AuthModule {
