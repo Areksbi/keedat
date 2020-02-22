944f0fe0745f9c7a06898c296b672d16d0a2ac20
Areksbi <36035819+Areksbi@users.noreply.github.com>
2020-02-23 00:26:14 +0100

    Fix auth guard check

--- src/app/auth/_helpers/auth.guard.ts
+++ src/app/auth/_helpers/auth.guard.ts
@@ -1,8 +1,8 @@
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 
-import { iif, Observable, of } from 'rxjs';
-import { mergeMap } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 import { AuthFacade } from '../_store/facades/auth.facades';
 import { links } from '../../_constants/links.constant';
@@ -24,13 +24,13 @@ export class AuthGuard implements CanActivate {
   ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return this.authFacade.getIsAuth()
       .pipe(
-        mergeMap((isAuth: boolean) =>
-          iif(() =>
-            isAuth,
-            of(isAuth),
-            this.router.navigate([links.HOME], { queryParams: { returnUrl: state.url }})
-          )
-        )
+        switchMap((isAuth: boolean) => {
+          if (isAuth) {
+            return of(isAuth);
+          }
+          this.router.navigate([links.HOME], { queryParams: { returnUrl: state.url }});
+          return of(false);
+        })
       )
   }
 
