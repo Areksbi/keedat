ffa93c1f3789d0abd669f04b95f82077aa19eeab
Areksbi <36035819+Areksbi@users.noreply.github.com>
2020-04-05 15:03:18 +0200

    Add Cypress

--- src/app/account/_components/account-overview/account-overview.component.ts
+++ src/app/account/_components/account-overview/account-overview.component.ts
@@ -1,26 +1,26 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { AuthFacade } from '../../../auth/_store/facades/auth.facades';
-import { combineLatest, Observable, of, Subject } from 'rxjs';
-import { SpinnerFacades } from '../../../_store/facades';
-import { AccountService } from '../../_services/account.service';
-import { concatMap, first, takeUntil } from 'rxjs/operators';
-import { AuthService } from '../../../auth/_services/auth.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { mustNotMatchIfNotEmpty } from '../../../_helpers/must-not-match.validator';
-import { RequestUpdateAccount } from '../../_interfaces/account.interface';
-import { requestUpdateUser } from '../../../auth/_store/actions/auth.actions';
 import { Store } from '@ngrx/store';
+import { combineLatest, Observable, of, Subject } from 'rxjs';
+import { concatMap, first, map, takeUntil } from 'rxjs/operators';
+import { mustNotMatchIfNotEmpty } from '../../../_helpers/must-not-match.validator';
+import { SpinnerFacades } from '../../../_store/facades';
 import { State } from '../../../_store/reducers';
+import { AuthService } from '../../../auth/_services/auth.service';
+import { requestUpdateUser } from '../../../auth/_store/actions/auth.actions';
+import { AuthFacade } from '../../../auth/_store/facades/auth.facades';
+import { RequestUpdateAccount } from '../../_interfaces/account.interface';
+import { AccountService } from '../../_services/account.service';
 
 @Component({
   selector: 'app-account-overview',
   templateUrl: './account-overview.component.html',
-  styleUrls: ['./account-overview.component.scss']
+  styleUrls: ['./account-overview.component.scss'],
 })
 export class AccountOverviewComponent implements OnInit, OnDestroy {
-  public hiddenPassword: boolean = true;
-  public hiddenNewPassword: boolean = true;
-  public isAccountUpdating: boolean = false;
+  public hiddenPassword = true;
+  public hiddenNewPassword = true;
+  public isAccountUpdating = false;
   public isLoading$: Observable<boolean>;
   public updateForm: FormGroup;
   private readonly onDestroy$ = new Subject<boolean>();
@@ -31,23 +31,25 @@ export class AccountOverviewComponent implements OnInit, OnDestroy {
     private authService: AuthService,
     private formBuilder: FormBuilder,
     private spinnerFacade: SpinnerFacades,
-    private store: Store<State>,
-  ) { }
+    private store: Store<State>
+  ) {}
 
   public ngOnInit() {
     this.isLoading$ = this.spinnerFacade.isLoading();
-    this.updateForm = this.formBuilder.group({
-      email: ['', [Validators.email, Validators.required]],
-      password: ['', Validators.minLength(8)],
-      newPassword: ['', Validators.minLength(8)]
-    }, {
-      validator: mustNotMatchIfNotEmpty('password', 'newPassword')
-    });
-    this.authFacade.getEmail()
-      .pipe(
-        takeUntil(this.onDestroy$)
-      )
-      .subscribe((email: string) => this.updateForm.controls['email'].setValue(email));
+    this.updateForm = this.formBuilder.group(
+      {
+        email: ['', [Validators.email, Validators.required]],
+        password: ['', Validators.minLength(8)],
+        newPassword: ['', Validators.minLength(8)],
+      },
+      {
+        validator: mustNotMatchIfNotEmpty('password', 'newPassword'),
+      }
+    );
+    this.authFacade
+      .getEmail()
+      .pipe(takeUntil(this.onDestroy$))
+      .subscribe((email: string) => this.updateForm.controls.email.setValue(email));
   }
 
   public ngOnDestroy(): void {
@@ -56,7 +58,8 @@ export class AccountOverviewComponent implements OnInit, OnDestroy {
   }
 
   public deleteAccount(): void {
-    this.authFacade.getUserId()
+    this.authFacade
+      .getUserId()
       .pipe(
         takeUntil(this.onDestroy$),
         first(Boolean),
@@ -66,45 +69,51 @@ export class AccountOverviewComponent implements OnInit, OnDestroy {
   }
 
   public updateAccount() {
-    if (this.updateForm.invalid || !this.updateForm.dirty)  {
+    if (this.updateForm.invalid || !this.updateForm.dirty) {
       return;
     }
 
-    this.authFacade.getEmail()
+    this.authFacade
+      .getEmail()
       .pipe(
         takeUntil(this.onDestroy$),
         first(Boolean),
         concatMap((email: string) =>
           combineLatest([
-              of(email),
-              of(this.updateForm.get('email')),
-              of(this.updateForm.get('password')),
-              of(this.updateForm.get('newPassword')),
-            ],
-          )),
-        first(([email, emailControl, password, newPassword]) =>
-          (emailControl.value && emailControl.value !== email)
-          || (password.value && newPassword.value && password.value !== newPassword.value)),
+            of(email),
+            of(this.updateForm.get('email')),
+            of(this.updateForm.get('password')),
+            of(this.updateForm.get('newPassword')),
+          ])
+        ),
+        first(
+          ([email, emailControl, password, newPassword]) =>
+            (emailControl.value && emailControl.value !== email) ||
+            (password.value && newPassword.value && password.value !== newPassword.value)
+        ),
         concatMap(([email, emailControl, password, newPassword]) => {
           const body: RequestUpdateAccount = {
-            email: emailControl.value !== email && emailControl.value || null,
+            email: (emailControl.value !== email && emailControl.value) || null,
             password: password.value || null,
-            newPassword: newPassword.value || null
+            newPassword: newPassword.value || null,
           };
 
           const hasProperties: boolean = Object.keys(body).map((key: string) => !body[key] && delete body[key]).length > 0;
 
           if (hasProperties) {
             return combineLatest([
-              this.authFacade.getUserId().pipe(first(Boolean)),
-              of(body)
+              this.authFacade.getUserId().pipe(
+                first(Boolean),
+                map((id: string) => id)
+              ),
+              of(body),
             ]);
           }
-        }),
+        })
       )
       .subscribe(([id, body]) => {
         this.store.dispatch(requestUpdateUser({ id, body }));
-        this.isAccountUpdating = false
-      })
+        this.isAccountUpdating = false;
+      });
   }
 }
