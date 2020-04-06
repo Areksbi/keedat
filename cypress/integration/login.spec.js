78a4dc169063e9a9693e05e1730e7c7067132524
Areksbi <36035819+Areksbi@users.noreply.github.com>
2020-04-07 00:39:52 +0200

    Add Cypress login check in local storage

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -7,14 +7,23 @@ describe('Login', () => {
     cy.get('.mat-tab-label').contains('Login').click()
   })
 
-  it('should login', () => {
+  it('should login and set user in local storage', () => {
     cy.get('[data-e2e="login-email"]').type(Cypress.config('email'))
     cy.get('[data-e2e="login-password"]').type(Cypress.config('password'))
     cy.get('[data-e2e="login-submit"]').click()
 
-    cy.wait('@login')
+    cy.wait('@login').then(() => {
+      cy.get('[data-e2e="header-logout"]').contains('Logout')
 
-    cy.get('[data-e2e="header-logout"]').contains('Logout')
+      expect(localStorage.getItem('email')).to.eq(Cypress.config('email'))
+      expect(localStorage.getItem('token')).to.have.lengthOf(211)
+
+      const getHours = date => Math.abs(new Date(date).getTime() - new Date().getTime()) / 3600000
+      expect(Math.abs(getHours(localStorage.getItem('expiration'))))
+        .to.be.greaterThan(0.999)
+        .to.be.lessThan(1)
+      expect(localStorage.getItem('userId')).to.eq(Cypress.config('userId'))
+    })
   })
 
   it('should NOT login with wrong email', () => {
