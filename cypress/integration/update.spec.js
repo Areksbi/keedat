696d226eeb166b7baab10805dfe4c1b9c189ae41
Areksbi <36035819+Areksbi@users.noreply.github.com>
2020-04-10 00:36:21 +0200

    Add Cypress update password and update email and password

--- cypress/integration/update.spec.js
+++ cypress/integration/update.spec.js
@@ -4,14 +4,17 @@ function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-const uniqueId = getRandomIntInclusive(0, 99999999)
-const email = `${uniqueId}-${Cypress.config('email')}`
+let uniqueId = getRandomIntInclusive(0, 99999999)
+let email = `${uniqueId}-${Cypress.config('email')}`
 const password = 'password'
 
 describe('Login', () => {
   beforeEach(() => {
     cy.server()
     cy.route('PUT', `**/update/*`).as('update')
+
+    uniqueId = getRandomIntInclusive(0, 99999999)
+    email = `${uniqueId}-${Cypress.config('email')}`
   })
 
   it('should update user email', () => {
@@ -28,10 +31,55 @@ describe('Login', () => {
       expect(localStorage.getItem('email')).to.eq(`2-${email}`)
 
       cy.get('[data-e2e="header-logout"]').contains('Logout').click()
-
       cy.get('.mat-tab-label').contains('Login')
 
       cy.login(`2-${email}`, password)
+
+      cy.get('[data-e2e="header-logout"]').contains('Logout')
+      expect(localStorage.getItem('email')).to.eq(`2-${email}`)
+    })
+  })
+
+  it('should update user password', () => {
+    cy.register(email, password)
+    cy.login(email, password)
+
+    cy.get('[data-e2e="header-account"]').click()
+    cy.get('[data-e2e="enable-update"]').click()
+
+    cy.get('[data-e2e="update-password"]').clear().type(password)
+    cy.get('[data-e2e="update-new-password"]').clear().type(`2-${password}`)
+    cy.get('[data-e2e="update-account"]').click()
+
+    cy.wait('@update').then(() => {
+      cy.get('[data-e2e="header-logout"]').contains('Logout').click()
+      cy.get('.mat-tab-label').contains('Login')
+
+      cy.login(email, `2-${password}`)
+
+      cy.get('[data-e2e="header-logout"]').contains('Logout')
+    })
+  })
+
+  it('should update user email and password', () => {
+    cy.register(email, password)
+    cy.login(email, password)
+
+    cy.get('[data-e2e="header-account"]').click()
+    cy.get('[data-e2e="enable-update"]').click()
+
+    cy.get('[data-e2e="update-email"]').clear().type(`2-${email}`)
+    cy.get('[data-e2e="update-password"]').clear().type(password)
+    cy.get('[data-e2e="update-new-password"]').clear().type(`2-${password}`)
+    cy.get('[data-e2e="update-account"]').click()
+
+    cy.wait('@update').then(() => {
+      expect(localStorage.getItem('email')).to.eq(`2-${email}`)
+
+      cy.get('[data-e2e="header-logout"]').contains('Logout').click()
+      cy.get('.mat-tab-label').contains('Login')
+
+      cy.login(`2-${email}`, `2-${password}`)
 
       cy.get('[data-e2e="header-logout"]').contains('Logout')
       expect(localStorage.getItem('email')).to.eq(`2-${email}`)
