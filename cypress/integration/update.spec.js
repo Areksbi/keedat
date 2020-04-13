5524d83160e99c5b6ea6ea0a8170d3a4c01b26fd
Areksbi <36035819+Areksbi@users.noreply.github.com>
2020-04-13 14:52:48 +0200

    Update Cypress update

--- cypress/integration/update.spec.js
+++ cypress/integration/update.spec.js
@@ -16,7 +16,7 @@ describe('Login', () => {
     cy.route('PUT', `**/update/*`).as('update')
   })
 
-  it.only('should update user email', () => {
+  it('should update user email', () => {
     cy.register(email, password)
     cy.login(email, password)
 
@@ -83,5 +83,59 @@ describe('Login', () => {
       cy.get('[data-e2e="header-logout"]').contains('Logout')
       expect(localStorage.getItem('email')).to.eq(`2-${email}`)
     })
+  })
+
+  it('should NOT update user email if the same but password', () => {
+    cy.register(email, password)
+    cy.login(email, password)
+
+    cy.get('[data-e2e="header-account"]').click()
+    cy.get('[data-e2e="enable-update"]').click()
+
+    cy.get('[data-e2e="update-email"]').clear().type(email)
+    cy.get('[data-e2e="update-password"]').clear().type(password)
+    cy.get('[data-e2e="update-new-password"]').clear().type(`2-${password}`)
+    cy.get('[data-e2e="update-account"]').click()
+
+    cy.wait('@update').then(() => {
+      expect(localStorage.getItem('email')).to.eq(email)
+
+      cy.get('[data-e2e="header-logout"]').contains('Logout').click()
+      cy.get('.mat-tab-label').contains('Login')
+
+      cy.login(email, `2-${password}`)
+
+      cy.get('[data-e2e="header-logout"]').contains('Logout')
+      expect(localStorage.getItem('email')).to.eq(email)
+    })
+  })
+
+  it('should NOT update if everything is empty', () => {
+    cy.register(email, password)
+    cy.login(email, password)
+
+    cy.get('[data-e2e="header-account"]').click()
+    cy.get('[data-e2e="enable-update"]').click()
+
+    cy.get('[data-e2e="update-email"]').clear()
+    cy.get('[data-e2e="update-password"]').clear()
+    cy.get('[data-e2e="update-new-password"]').clear()
+
+    cy.get('[data-e2e="update-account"]').should('be.disabled')
+  })
+
+  it('should NOT update if password is the same', () => {
+    cy.register(email, password)
+    cy.login(email, password)
+
+    cy.get('[data-e2e="header-account"]').click()
+    cy.get('[data-e2e="enable-update"]').click()
+
+    cy.get('[data-e2e="update-password"]').clear().type(password)
+    cy.get('[data-e2e="update-new-password"]').clear().type(password)
+
+    cy.get('[data-e2e="update-password"]').focus()
+    cy.get('mat-error').contains('Enter a valid different password')
+    cy.get('[data-e2e="update-account"]').should('be.disabled')
   })
 })
